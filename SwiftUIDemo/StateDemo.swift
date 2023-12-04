6683437046ddc6db4d1b59cb43b88c09c70dcc9f
rodoolfogonzalez <rodolfo.swift.dev@gmail.com>
2023-12-04 05:17:59 -0300

    Add content

---
+++ SwiftUIDemo/StateDemo.swift
@@ -0,0 +1,34 @@
+//
+//  StateDemo.swift
+//  SwiftUIDemo
+//
+//  Created by rodoolfo gonzalez on 04-12-23.
+//
+
+import SwiftUI
+
+struct StateDemo: View {
+    
+    @State var contador : Int = 0
+    
+    var body: some View {
+        
+        //las vistas en SwiftUI son creadas a traves de Struct, de tipos de valor, para poder mutar sus propiedades usamos el propertyWrapper @State.
+        //con tan solo anteponer @State antes de var facutamos a la propiedad para que cambie y cumpla la funcion de regenerar la vista cuando esto suceda
+        Text("\(contador) : Pulsaciones")
+        
+        Button("Añadir"){
+            contador += 1
+        }
+        
+        Button("Restar"){
+            contador -= 1
+        }
+        .foregroundColor(.red)
+        
+    }
+}
+
+#Preview {
+    StateDemo()
+}

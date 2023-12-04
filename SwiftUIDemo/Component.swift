1fddb135cd8be1a9c9901e0473c70af5d28a40c3
rodoolfogonzalez <rodolfo.swift.dev@gmail.com>
2023-12-04 04:46:02 -0300

    Update Component file

---
+++ SwiftUIDemo/Component.swift
@@ -0,0 +1,24 @@
+//
+//  Component.swift
+//  SwiftUIDemo
+//
+//  Created by rodoolfo gonzalez on 03-12-23.
+//
+
+import Foundation
+
+
+//-SwitUIComponents
+
+//1-AxisStack
+//-ZStack
+//-VStack
+//-HStack
+
+//2-Text
+
+//3-Image
+
+//4-Icons And SFSymbols
+
+//5-Label
